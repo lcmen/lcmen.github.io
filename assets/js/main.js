4a8d471c46ebb59ad5e1146eb145a9738e730c6a
Lucas Mendelowski <lucas@mendelowski.com>
2023-10-16 20:30:07 +0200

    Update icons and add Turbo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,38 @@
+class Menu {
+  #button;
+  #container;
+  #open = false;
+
+  constructor(button) {
+    this.#button = button;
+    this.#container = button.querySelector('nav');
+    this.#button.addEventListener('click', this.toggle.bind(this));
+  }
+
+  toggle(e) {
+    e.preventDefault();
+
+    this.#open ? this.close() : this.open();
+  }
+
+  open() {
+    this.#open = true;
+    this.#button.classList.add('active');
+    this.#container.classList.remove('opacity-0', 'pointer-events-none');
+  }
+
+  close() {
+    this.#open = false;
+    this.#button.classList.remove('active');
+    this.#container.classList.add('opacity-0', 'pointer-events-none');
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#toggle-menu').addEventListener('click', function(e) {
-    e.currentTarget.classList.toggle('active');
-    const container = document.querySelector('#menu');
-    container.classList.toggle('opacity-0');
-    container.classList.toggle('pointer-events-none');
-  });
+  const button = document.querySelector('#menu');
+  window.menu = new Menu(button);
+});
+
+document.addEventListener('turbo:before-visit', function() {
+  window.menu.close();
 });
