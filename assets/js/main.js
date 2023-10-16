2208b57ada593b754cf3cb5fcb076fd5fdc7d087
Lucas Mendelowski <lucas@mendelowski.com>
2023-10-16 21:24:49 +0200

    Improve Turbo

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,12 @@ class Menu {
 
   constructor(button) {
     this.#button = button;
-    this.#container = button.querySelector('nav');
+    this.#container = button.nextElementSibling;
     this.#button.addEventListener('click', this.toggle.bind(this));
   }
 
   toggle(e) {
     e.preventDefault();
-
     this.#open ? this.close() : this.open();
   }
 
@@ -21,8 +20,13 @@ class Menu {
     this.#container.classList.remove('opacity-0', 'pointer-events-none');
   }
 
-  close() {
+  close(callback) {
     this.#open = false;
+
+    if (callback) {
+      this.#container.addEventListener('transitionend', callback, { once: true });
+    }
+
     this.#button.classList.remove('active');
     this.#container.classList.add('opacity-0', 'pointer-events-none');
   }
@@ -33,6 +37,15 @@ document.addEventListener('DOMContentLoaded', function() {
   window.menu = new Menu(button);
 });
 
-document.addEventListener('turbo:before-visit', function() {
+document.addEventListener('turbo:before-render', function(event) {
+  if (!document.startViewTransition) return;
+
+  event.preventDefault();
+  document.startViewTransition(function() {
+    event.detail.resume();
+  });
+});
+
+document.addEventListener('turbo:render', function() {
   window.menu.close();
 });
