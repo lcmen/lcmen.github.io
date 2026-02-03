5774d1c7e98953ee05d34df1afdafdc87a7da7fd
Lucas Mendelowski <lucas@mendelowski.com>
2026-02-03 19:56:50 +0100

    Upgrade Tailwind to V4

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,7 @@ const production = process.env.JEKYLL_ENV == 'production'
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
+    require('@tailwindcss/postcss'),
     ...(production ? [require('cssnano')({ preset: 'default' })] : [])
   ]
 }
